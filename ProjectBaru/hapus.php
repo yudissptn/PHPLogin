55ac81a447542c4c07b660911f49506073868a93
Yudhistira Septian Sampoerno <yudissptn@gmail.com>
2019-03-24 07:23:12 +0700

    Update hapus.php

--- ProjectBaru/hapus.php
+++ ProjectBaru/hapus.php
@@ -3,4 +3,5 @@ $id = $_GET['id'];
 $conn  = mysqli_connect("127.0.0.1","root","","Bebas");
 mysqli_query($conn,"delete from info_siswa where id_siswa='$id'");
 header("location: http://localhost/ProjectBaru/index.php");
+echo "aa";
  ?>
